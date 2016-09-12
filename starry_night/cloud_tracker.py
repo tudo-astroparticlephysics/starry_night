76dfaa106b2e0de6b6769f38d6a8c841d1258621
Solarer <solarer@hotmail.de>
2016-09-12 02:43:21 +0200

    cloud tracking looks promising. but direction inverted

--- starry_night/cloud_tracker.py
+++ starry_night/cloud_tracker.py
@@ -5,6 +5,7 @@ import logging
 from skimage.segmentation import active_contour
 from IPython import embed
 from starry_night.skycam import r2theta, theta2r
+from starry_night import skycam
 
 class Cloud:
     def __init__(self, _id):
@@ -28,26 +29,31 @@ class Cloud:
                 )
 
 class CloudTracker:
-    maxMaps = 5
+    max_maps = 5
     atmosphere_height = 9
-    def __init__(self, conf):
+    def __init__(self, config):
         self.log = logging.getLogger(__name__)
         self.maps = []
-        self.transMaps= []
+        self.trans_maps= []
+        self.pred_map = []
         self.maxID = 0
         self.wind_speed = None
-        self.wind_direction = None
-        self.config = conf
+        self.wind_direction = []
+        self.crop = None
+        self.trans_crop = None
+        self.config = config
 
     def __add_cloud(self, cloudMap):
+        if not self.maps:
+            self.crop = skycam.get_crop_mask(cloudMap, self.config['crop'])
+            self.trans_crop = self.__transform_map(self.crop)
+        cloudMap[self.crop] = np.NaN
         self.maps.append(cloudMap)
-        self.transMaps.append(self.__transformMap(cloudMap))
+        self.trans_maps.append(self.__transform_map(cloudMap))
         self.maxID += 1
-        if (len(self.maps) > self.maxMaps):
-            self.maps.pop(0)
-            self.transMaps.pop(0)
+        
 
-    def __transformMap(self, cloudMap):
+    def __transform_map(self, cloudMap):
         '''
         Project fish eye lens image onto flat atmosphere
         transformed = np.zeros(1000,1000)
@@ -65,7 +71,7 @@ class CloudTracker:
         pixel_dist = np.sqrt((Y-transformed.shape[1]//2)**2 + (X-transformed.shape[0]//2)**2)
         theta = np.arctan(pixel_dist/np.max(pixel_dist)*4)
         phi = np.arctan2(Y-transformed.shape[1]//2,X-transformed.shape[0]//2)
-        r = theta2r(theta, float(self.config['radius']), self.config['angleProjection'])
+        r = theta2r(theta, float(self.config['image']['radius']), self.config['image']['angleProjection'])
         x,y = r * np.sin(phi) + cloudMap.shape[1]//2, r*np.cos(phi) + cloudMap.shape[0]//2
         return cloudMap[x.astype(int),y.astype(int)]
 
@@ -77,10 +83,11 @@ class CloudTracker:
 
     def __calculate_movement(self, map1, map2):
         self.log.debug('Calculate cloud movement')
+        print('calc')
         minVal = np.inf
         rotation = (0,0)
         for i,j in product(np.arange(0, map1.shape[0], map1.shape[0]//20), np.arange(0, map1.shape[1], map1.shape[1]//20)):
-            temp = np.sum((map1 - np.roll(np.roll(map2, i, axis=0), j, axis=1))**2)
+            temp = np.nansum((map1 - np.roll(np.roll(map2, i, axis=0), j, axis=1))**2)
             if temp < minVal:
                 minVal = temp
                 rotation = (i,j)
@@ -92,22 +99,35 @@ class CloudTracker:
 
     def print_clouds(self):
         print('Currently {} images loaded'.format(len(self.maps)))
-        for i,j in zip(self.maps, self.transMaps):
-            fig = plt.figure()
-            ax1 = fig.add_subplot(121)
-            ax1.imshow(i, vmin=0, vmax=1)
-            ax2 = fig.add_subplot(122)
-            ax2.imshow(j, vmin=0, vmax=1)
-            plt.show()
+        fig = plt.figure(figsize=(16,9))
+        for cnt, (i,j) in enumerate(zip(self.maps, self.trans_maps)):
+            print(100*len(self.maps)+20 + 2*cnt+1,100*len(self.maps)+20 + 2*cnt+2)
+            fig.add_subplot(len(self.maps), 2, 2*cnt+1)
+            plt.gca().imshow(i, vmin=0, vmax=1)
+            fig.add_subplot(len(self.trans_maps), 2, 2*cnt+2)
+            try:
+                plt.gca().imshow(self.pred_map[cnt+2], vmin=0, vmax=1)
+            except:
+                pass
+        plt.tight_layout()
+        plt.show()
 
     # update detects clouds in cloudmap and updates old cloud positions
     def update(self, cloudMap):
+        print('update')
         self.log.debug('Update')
         # store projection of clouds on atmosphere
         self.__add_cloud(cloudMap)
 
         if len(self.maps) > 1:
-            print(self.__calculate_movement(self.transMaps[-2], self.transMaps[-1]))
+            direct, val = self.__calculate_movement(self.trans_maps[-2], self.trans_maps[-1])
+            self.wind_direction.append(direct)
+            self.pred_map.append(np.roll(np.roll(self.maps[-1], self.wind_direction[-1][0], axis=0), self.wind_direction[-1][1], axis=1))
+
+        if (len(self.maps) > self.max_maps):
+            self.maps.pop(0)
+            self.trans_maps.pop(0)
+            #self.wind_direction.pop(0)
 
 
 
